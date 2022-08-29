90c1c756b885879d1dfdaf2d21b382aad0c4fb0e
venusurampudi <svsvenu@gmail.com>
2022-08-29 17:21:13 -0500

    Added asynchronous client to call back inbound

---
+++ src/main/java/com/dais/ioi/external/config/client/IOIActionClient.java
@@ -0,0 +1,21 @@
+package com.dais.ioi.external.config.client;
+
+import com.dais.ioi.action.domain.dto.internal.InboundResponseDataDto;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.Valid;
+
+@FeignClient( "ioi-action-service" )
+public interface IOIActionClient
+{
+    @ResponseStatus( HttpStatus.NO_CONTENT )
+    @RequestMapping( value = "/pub/v1/inbound",
+                     method = RequestMethod.POST )
+    @ApiOperation( value = "Inbound API" )
+    public void processInboundData( final @Valid InboundResponseDataDto inboundPublicData );
+}
