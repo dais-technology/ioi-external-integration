152058d6a5ee2e88e32476964db677371e17e3b5
Jason Demeter <jasondemeter@Jason-Demeters-MacBook-Pro.local>
2022-09-06 16:30:48 -0500

    WIP

--- src/main/java/com/dais/ioi/external/service/hubspot/HubSpotService.java
+++ src/main/java/com/dais/ioi/external/service/hubspot/HubSpotService.java
@@ -2,11 +2,8 @@ package com.dais.ioi.external.service.hubspot;
 
 import com.dais.ioi.external.domain.dto.hubspot.HubspotTrackRequest;
 
-import java.util.Map;
-
 
 public interface HubSpotService
 {
-    //todo: replace parameters with proper DTO
     void trackEvent( final HubspotTrackRequest request );
 }
