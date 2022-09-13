c587a276804874014c0c163f5a1e75b9f87d727b
Scott Fredrick <42282102+ScottFredrickDais@users.noreply.github.com>
2022-09-13 09:38:16 -0500

    WIP (#17)
    Adjust CA/SA non-quote integration to be a synchronous call more similar to async quoting endpoint

--- src/main/java/com/dais/ioi/external/service/ExternalIntegrationServiceImpl.java
+++ src/main/java/com/dais/ioi/external/service/ExternalIntegrationServiceImpl.java
@@ -4,6 +4,7 @@ import com.dais.ioi.action.domain.dto.FiredTriggerDto;
 import com.dais.ioi.action.domain.dto.pub.TriggerResponseDto;
 import com.dais.ioi.external.domain.dto.IntegrationDto;
 import com.dais.ioi.external.domain.dto.hubspot.HubspotTrackRequest;
+import com.dais.ioi.external.domain.dto.internal.enums.IntegrationType;
 import com.dais.ioi.external.domain.dto.jm.CreateAccountRequest;
 import com.dais.ioi.external.domain.dto.jm.CreateAccountResponse;
 import com.dais.ioi.external.domain.dto.jm.SubmitApplicationRequest;
@@ -14,6 +15,7 @@ import com.dais.ioi.external.service.action.jm.JMCreateAccountServiceImpl;
 import com.dais.ioi.external.service.action.jm.JMQuoteServiceImpl;
 import com.dais.ioi.external.service.hubspot.HubSpotService;
 import com.dais.ioi.external.service.action.jm.JMSubmitApplicationServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFacade;
@@ -21,6 +23,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Map;
 import java.util.UUID;
 
 
@@ -66,11 +70,41 @@ public class ExternalIntegrationServiceImpl
     }
 
 
+    // TODO Temporarily hardcoded and probably mis-located until we have time to re-architect and refactor
+    @Override
+    public TriggerResponseDto processSynchronous( final FiredTriggerDto firedTriggerDto )
+          throws Exception
+    {
+        TriggerResponseDto triggerResponseDto = new TriggerResponseDto();
+        final String externalIntegrationType = (String) firedTriggerDto.getPayload().getOrDefault( "externalIntegrationType", null );
+        final IntegrationType integrationType = IntegrationType.valueOf( externalIntegrationType );
+
+        if (integrationType == IntegrationType.JM_CREATE_ACCOUNT)
+        {
+            final UUID jmQuoteId = UUID.fromString( (String) firedTriggerDto.getPayload().get( "jmQuoteId" ) );
+            final CreateAccountRequest createAccountRequest = new CreateAccountRequest( jmQuoteId );
+            final CreateAccountResponse createAccountResponse = createAccountService.createAccount( createAccountRequest, firedTriggerDto.getLineId() );
+            triggerResponseDto.setMetadata( new ObjectMapper().convertValue( createAccountResponse, Map.class ) );
+        }
+        else if (integrationType == IntegrationType.JM_SUBMIT_APPLICATION)
+        {
+            final UUID jmQuoteId = UUID.fromString( (String) firedTriggerDto.getPayload().get( "jmQuoteId" ) );
+            final BigDecimal totalAmount = BigDecimal.valueOf((Double) firedTriggerDto.getPayload().get( "totalAmount" ));
+            final SubmitApplicationRequest submitApplicationRequest = new SubmitApplicationRequest( jmQuoteId, totalAmount );
+            final SubmitApplicationResponse submitApplicationResponse = jmSubmitApplication.submit( submitApplicationRequest, firedTriggerDto.getLineId() );
+            triggerResponseDto.setMetadata( new ObjectMapper().convertValue( submitApplicationResponse, Map.class ) );
+        }
+
+        return triggerResponseDto;
+    }
+
+
     @Override
     public void hubspotTrack( final HubspotTrackRequest request )
     {
         hubSpotService.trackEvent( request );
     }
+
 
     @Override
     public SubmitApplicationResponse submitApplication(final SubmitApplicationRequest submitApplicationRequest, final UUID orgId ) {
