82e6952276731349f5e61956fbf1a951e6ea2419
Jason Demeter <jasondemeter@Jason-Demeters-MacBook-Pro.local>
2022-09-13 10:51:40 -0500

    IOIENH-478 JsonPathPropertiesMapper

--- src/main/java/com/dais/ioi/external/util/JsonPathPropertiesMapperUtil.java
+++ src/main/java/com/dais/ioi/external/util/JsonPathPropertiesMapperUtil.java
@@ -1,5 +1,41 @@
 package com.dais.ioi.external.util;
 
+import com.jayway.jsonpath.JsonPath;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class JsonPathPropertiesMapperUtil
 {
+
+    public Map<String, String> map( final Map<String, String> properties,
+                                    final Map source )
+    {
+        Map<String, String> returnProperties = new HashMap<>();
+        for ( String property : properties.keySet() )
+        {
+            String jsonPath = properties.get( property );
+            Object value = JsonPath.read( source, jsonPath );
+            if ( value instanceof String )
+            {
+                returnProperties.put( property, (String) value );
+            }
+            if ( value instanceof Collection )
+            {
+                Collection values = (Collection) value;
+                int i = 1;
+                for ( Object item : values )
+                {
+                    if ( item instanceof String )
+                    {
+                        returnProperties.put( property + "_" + i, (String) item );
+                        i++;
+                    }
+                }
+            }
+        }
+        return returnProperties;
+    }
 }
--- src/test/java/com/dais/ioi/external/util/JsonPathPropertiesMapperUtilTest.java
+++ src/test/java/com/dais/ioi/external/util/JsonPathPropertiesMapperUtilTest.java
@@ -3,7 +3,10 @@ package com.dais.ioi.external.util;
 import com.dais.ioi.external.FileReader;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class JsonPathPropertiesMapperUtilTest
@@ -14,17 +17,48 @@ public class JsonPathPropertiesMapperUtilTest
 
     @Test
     public void test()
+          throws IOException
     {
         Map source = givenASourceObject();
         Map<String, String> properties = givenJsonPathProperties();
-        JsonPathPropertiesMapperUtil mapper = givenAJsonPathPropertiesMapperUtil( properties, source );
-        Map<String, String> result = whenTheMapperMaps();
+        JsonPathPropertiesMapperUtil mapper = givenAJsonPathPropertiesMapperUtil();
+        Map<String, String> result = whenTheMapperMaps( mapper, properties, source );
         thenTheResultContainsAllMappedFields( result );
     }
 
 
-    private Map givenASourceObject()
+    private void thenTheResultContainsAllMappedFields( final Map<String, String> actual )
+          throws IOException
     {
-        return null;
+        Map expected = reader.loadFileAsMap( "util/jsonPathPropertiesMapperUtilTestData/jsonPathExpectedOutput.json" );
+        assertEquals( expected, actual );
+    }
+
+
+    private Map<String, String> whenTheMapperMaps( final JsonPathPropertiesMapperUtil mapper,
+                                                   final Map<String, String> properties,
+                                                   final Map source )
+    {
+        return mapper.map( properties, source );
+    }
+
+
+    private JsonPathPropertiesMapperUtil givenAJsonPathPropertiesMapperUtil()
+    {
+        return new JsonPathPropertiesMapperUtil();
+    }
+
+
+    private Map<String, String> givenJsonPathProperties()
+          throws IOException
+    {
+        return reader.loadFileAsMap( "util/jsonPathPropertiesMapperUtilTestData/jsonPathProperties.json" );
+    }
+
+
+    private Map givenASourceObject()
+          throws IOException
+    {
+        return reader.loadFileAsMap( "util/jsonPathPropertiesMapperUtilTestData/sourceData.json" );
     }
 }
