75ad308c85a28a451fca1be11b49c117331919fc
Jason Demeter <jasondemeter@Jason-Demeters-MacBook-Pro.local>
2022-09-28 10:39:59 -0500

    WIP

--- src/main/java/com/dais/ioi/external/domain/api/HubspotApi.java
+++ src/main/java/com/dais/ioi/external/domain/api/HubspotApi.java
@@ -3,9 +3,12 @@ package com.dais.ioi.external.domain.api;
 import com.dais.ioi.external.domain.dto.hubspot.HubspotTrackRequest;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.Valid;
 
 
 public interface HubspotApi
@@ -14,5 +17,5 @@ public interface HubspotApi
     @RequestMapping( value = "/hubspot",
                      method = RequestMethod.POST )
     @ApiOperation( value = "make a track call to hubspot" )
-    void trackEvent( HubspotTrackRequest request );
+    void trackEvent( @RequestBody @Valid HubspotTrackRequest request );
 }
