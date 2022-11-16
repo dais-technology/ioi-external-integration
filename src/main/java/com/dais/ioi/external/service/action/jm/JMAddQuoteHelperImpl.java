ed8a1c3217ef455846e0b36ec328297fa42fe15c
vladanesic <94044166+vladanesic@users.noreply.github.com>
2022-11-16 16:31:13 +0100

    IOIENH-735 - move gender to item (#122)

--- src/main/java/com/dais/ioi/external/service/action/jm/JMAddQuoteHelperImpl.java
+++ src/main/java/com/dais/ioi/external/service/action/jm/JMAddQuoteHelperImpl.java
@@ -965,9 +965,9 @@ public class JMAddQuoteHelperImpl
                   getValue( () -> clientLoopIterationDto.getAnswers().get( actionJMSQuoteSpecDto.getLastAppraisalDate() ).getAnswer(), "" )
             );
 
-            //            item.setGender(
-            //                  getValue( () -> clientLoopIterationDto.getAnswers().get( actionJMSQuoteSpecDto.getItemGender() ).getAnswer(), "" )
-            //            );
+            item.setGender(
+                  getValue( () -> clientLoopIterationDto.getAnswers().get( actionJMSQuoteSpecDto.getItemGender() ).getAnswer(), "" )
+            );
           /*  item.setItemDamage(
                   getValue( () -> clientLoopIterationDto.getAnswers().get( actionJMSQuoteSpecDto.getItemDamage() ).getAnswer(), "" ).toString()
             );
@@ -1000,10 +1000,6 @@ public class JMAddQuoteHelperImpl
                 }
                 primaryWearer = nonContactWearer;
             }
-
-            primaryWearer.setGender(
-                  getValue( () -> clientLoopIterationDto.getAnswers().get( actionJMSQuoteSpecDto.getItemGender() ).getAnswer().toString(), "" )
-            );
 
             AddQuoteRequest.DeductibleOption deductibleOption = new AddQuoteRequest.DeductibleOption();
 
@@ -1089,10 +1085,6 @@ public class JMAddQuoteHelperImpl
               getValue( () -> wearerDto.getAnswers().get( actionJMSQuoteSpecDto.getPrimaryWearerPhoneNumber() ).getAnswer().toString(), "" )
         );
 
-        primaryWearer.setGender(
-              getValue( () -> wearerDto.getAnswers().get( actionJMSQuoteSpecDto.getItemGender() ).getAnswer().toString(), "" )
-        );
-
         AddQuoteRequest.ResidentialAddress primaryWearerResidentialAddress = new AddQuoteRequest.ResidentialAddress();
 
         primaryWearerResidentialAddress.setAddress1(
@@ -1153,10 +1145,6 @@ public class JMAddQuoteHelperImpl
 
         primaryWearer.setEmailAddress(
               getValue( () -> wearerDto.getAnswers().get( actionJMSQuoteSpecDto.getPrimaryContactEmail() ).getAnswer().toString(), "" )
-        );
-
-        primaryWearer.setGender(
-              getValue( () -> wearerDto.getAnswers().get( actionJMSQuoteSpecDto.getItemGender() ).getAnswer().toString(), "" )
         );
 
         primaryWearer.setRelationWithApplicant(
