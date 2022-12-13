ed26561e1bcbe79d369ea8a680cfa5eb03708825
Jason Demeter <jasondemeter@Jason-Demeters-MacBook-Pro.local>
2022-12-13 13:25:06 -0600

    WIP

--- src/main/java/com/dais/ioi/external/config/FeignClientsConfig.java
+++ src/main/java/com/dais/ioi/external/config/FeignClientsConfig.java
@@ -2,6 +2,7 @@ package com.dais.ioi.external.config;
 
 import feign.Logger;
 import feign.RequestInterceptor;
+import feign.Retryer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
